5bbb245b8e6766194093d460a6a29edadb9c454c
Darek Radke <dradmclad@gmail.com>
2023-11-21 16:00:40 -0600

    copy for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,27 @@
+import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import Laptop from "../../public/images/laptop.jpg";
 
 export default function Home() {
   return (
-    
-    <header className="header-wrapper">
-      <p className="header-name">
-      Darek Radke
+    <>
+      <header className="flex justify-center items-center min-w-full h-20">
+        <p>Darek Radke</p>
+      </header>
+      <Image src={Laptop} alt="pfp" width={500} />
+      <p>
+        I am a website developer that enjoys working with small businesses to
+        create responsive modern websites. The technologies I use most often
+        are: Next.js, React.js, Typescript, Node.js, and Tailwind CSS.
       </p>
-    </header>
+      <header className="flex justify-center items-center min-w-full h-20">
+        Contact
+      </header>
+      <header>
+        Websites
+      </header>
 
-  )
+    </>
+  );
 }
