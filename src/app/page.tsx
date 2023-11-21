553686480513fe0e1f310188ed0e7bd79499fd35
Darek Radke <dradmclad@gmail.com>
2023-11-21 14:50:27 -0600

    dependencies

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
-
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   return (
-    <>
-    </>
+    
+    <header className="header-wrapper">
+      <p className="header-name">
+      Darek Radke
+      </p>
+    </header>
+
   )
 }
